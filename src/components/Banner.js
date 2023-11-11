440db393f466dbd34a8c1cdfa03100e8007b94b2
kayFley <vasya.shumilov.2003@gmail.com>
2023-11-11 18:37:11 +0700

    Banner and navbar design update

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,17 +1,58 @@
+import { useState, useEffect } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import { ArrowRightCircle } from "react-bootstrap-icons";
-import headerImg from "../assets/img/header-img.svg";
+// import headerImg from "../assets/img/header-img.svg";
 
 export const Banner = () => {
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const toRotate = ["Frontend Developer", "Backend Developer", "Web Developer"];
+  const [text, setText] = useState("");
+  const [delta, setDelta] = useState(150 - Math.random() * 100);
+  const period = 2000;
+
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+
+    return () => {
+      clearInterval(ticker);
+    };
+  }, [text]);
+
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updateText = isDeleting
+      ? fullText.substring(0, text.length - 1)
+      : fullText.substring(0, text.length + 1);
+
+    setText(updateText);
+
+    if (isDeleting) {
+      setDelta((prevDelta) => prevDelta / 2);
+    }
+
+    if (!isDeleting && updateText === fullText) {
+      setIsDeleting(true);
+      setDelta(period);
+    } else if (isDeleting && updateText === "") {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setDelta(150);
+    }
+  };
+
   return (
     <section className="banner" id="home">
       <Container>
         <Row className="align-item-center">
           <Col xs={12} md={6} xl={7}>
-            <span className="tagline">Welcome to my portfolio</span>
+            <span className="tagline">kayFley's portfolio</span>
             <h1>
-              {`Hi Im webdecoded`}
-              <span className="wrap">web dev</span>
+              {`I'm `}
+              <span className="wrap">{text}</span>
             </h1>
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
@@ -27,7 +68,7 @@ export const Banner = () => {
             </button>
           </Col>
           <Col xs={12} md={6} xl={5}>
-            <img src={headerImg} alt="Header Img" />
+            <img src={""} alt="" />
           </Col>
         </Row>
       </Container>
