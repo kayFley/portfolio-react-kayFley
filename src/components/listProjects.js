c059d374c9f0f6c647fe1ea996434090eeb69278
kayFley <vasya.shumilov.2003@gmail.com>
2023-11-12 04:59:45 +0700

    pre-deployment

--- src/components/listProjects.js
+++ src/components/listProjects.js
@@ -1,9 +1,26 @@
+import backendBlogImg from "../assets/img/project-img1.png";
+
 const listProjects = [
   {
-    title: "",
-    description: "",
-    imgUrl: ,
-    url: "",
+    title: "Backend blog",
+    description:
+      "Express, Express-validator; MongoDB/Mongoose; JWT; Multer; BCrypt;",
+    imgUrl: backendBlogImg,
+    url: "https://github.com/kayFley/backend-nodejs-kayFley",
+  },
+  {
+    title: "Backend blog",
+    description:
+      "Express, Express-validator; MongoDB/Mongoose; JWT; Multer; BCrypt;",
+    imgUrl: backendBlogImg,
+    url: "https://github.com/kayFley/backend-nodejs-kayFley",
+  },
+  {
+    title: "Backend blog",
+    description:
+      "Express, Express-validator; MongoDB/Mongoose; JWT; Multer; BCrypt;",
+    imgUrl: backendBlogImg,
+    url: "https://github.com/kayFley/backend-nodejs-kayFley",
   },
 ];
 
