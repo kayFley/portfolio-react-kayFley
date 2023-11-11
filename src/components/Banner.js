f7ea21f44bc25172b9e264440065f392f20b0372
kayFley <vasya.shumilov.2003@gmail.com>
2023-11-11 16:31:14 +0700

    Creating the banner and navbar

---
+++ src/components/Banner.js
@@ -0,0 +1,36 @@
+import { Col, Container, Row } from "react-bootstrap";
+import { ArrowRightCircle } from "react-bootstrap-icons";
+import headerImg from "../assets/img/header-img.svg";
+
+export const Banner = () => {
+  return (
+    <section className="banner" id="home">
+      <Container>
+        <Row className="align-item-center">
+          <Col xs={12} md={6} xl={7}>
+            <span className="tagline">Welcome to my portfolio</span>
+            <h1>
+              {`Hi Im webdecoded`}
+              <span className="wrap">web dev</span>
+            </h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <button onClick={() => console.log("connect")}>
+              Connect <ArrowRightCircle size={25} />
+            </button>
+          </Col>
+          <Col xs={12} md={6} xl={5}>
+            <img src={headerImg} alt="Header Img" />
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
